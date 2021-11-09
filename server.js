f341ba761a4e4395c9df5e7cb0f654c63cccb416
rohansharma2023 <rohansh@live.unc.edu>
2021-11-09 15:55:25 -0500

    pushing installed dependencies and create new user method

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 var express = require("express")
 var app = express()
 // Require database SCRIPT file
-
+var db = require("./database.js") // Added this line of code.
 // Require md5 MODULE
-
+var md5 = require("md5") // Added this line of code.
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
-
+var HTTP_PORT = 5000 // Added this line of code.
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
@@ -23,7 +23,11 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-
+app.post("/app/new", (req, res) =>{                                                 // Went over this in lecture
+	const stmt = db.prepare("INSERT INTO userinfo (user,pass) VALUES (?, ?)");
+	const info = stmt.run(req.body.user, md5(req.body.pass));
+	res.status(201).json({"message": info.changes + "record created: ID" + info.lastInsertRowid});
+});
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
