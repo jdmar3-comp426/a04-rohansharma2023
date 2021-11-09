1624744043b9eaa07b7aa077d936ea3500295cda
rohansharma2023 <rohansh@live.unc.edu>
2021-11-09 17:20:54 -0500

    reverting server to notmal changed db to users.db again shouldnt make a diff

--- server.js
+++ server.js
@@ -26,10 +26,7 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new", (req, res) =>{                                                 // Went over this in lecture
 	const stmt = db.prepare("INSERT INTO userinfo (user,pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user, md5(req.body.pass));
-	res.json({"message": info.changes + " record created: ID " + info.lastInsertRowid + "( 201)"});
-	res.status(201);
-	
-	//res.status(201).json({"message": info.changes + " record created: ID " + info.lastInsertRowid});
+	res.status(201).json({"message": info.changes + " record created: ID " + info.lastInsertRowid});
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
