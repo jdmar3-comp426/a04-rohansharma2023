9f55dc49aff379ed4dd364743cbcc1dba8fbd508
rohansharma2023 <rohansh@live.unc.edu>
2021-11-11 16:47:19 -0500

    read a single user

--- server.js
+++ server.js
@@ -35,6 +35,10 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/users:id", (req, res) => {	
+	const stmt = db.prepare("SELECT FROM userinfo WHERE id = ?").all();
+	res.status(200).json(stmt);
+});
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
